6707702f75cf062db31693c7346b391efe76e7d9
“e <xsshfb@outlook.com>
2019-12-18 21:15:22 +0800

    第三天提交

--- src/script/index.js
+++ src/script/index.js
@@ -1,10 +1,11 @@
 import { Tool } from './tool.js';
 var tool = new Tool();
+
 class Navigation {
     constructor() {
-        this.Oli = document.querySelectorAll('.one');
-        this.Oman = document.querySelector('.man');
-        this.maskLayer = document.querySelector('.mask_layer');
+        this.Oli = tool.$('.one', 'all');
+        this.Oman = tool.$('.man');
+        this.maskLayer = tool.$('.mask_layer');
     }
     init() {
         let _this = this;
@@ -38,88 +39,114 @@ new Navigation().init();
 
 
 // 广告条
-// !function () {
-//     class Banner {
-//         constructor() {
-//             this.swiperSlide = document.querySelectorAll('.swiper-slide');
-//             this.swiperWrapper = document.querySelector('.swiper-wrapper');
-//         }
-//         init() {
-//             let _this = this;
-//             let s = 0;
-//             let l = this.swiperSlide[0].offsetWidth;
-//             // setInterval(function () {
-//             //     s = s - l;
-//             //     _this.swiperWrapper.style.left = s + 'px';
-//             // }, 2000)
-//         }
 
+class Banner {
+    constructor() {
 
-//     }
-//     new Banner().init();
-// }()
+    }
+    init() {
+
+    }
+}
+new Banner().init();
 
 //幻灯片
 class Slide {
     constructor() {
-        this.btnleft = document.querySelector('.slide .lef');
-        this.btnright = document.querySelector('.slide .rig');
-        this.meau = document.querySelectorAll('.meau li');
-        this.img = document.querySelectorAll('.slide .pic img');
+        this.btnleft = tool.$('.slide .lef');
+        this.btnright = tool.$('.slide .rig');
+        this.meau = tool.$('.meau li', 'all');
+        this.img = tool.$('.slide .pic img', 'all');
         this.index = 0;
     }
     init() {
+        let _this = this;
         this.over();
-        this.click();
+        this.automation();
+        this.btnleft.onclick = function () {
+            _this.leftclick();
+        };
+        this.btnright.onclick = function () {
+            _this.rightclick();
+        };
     }
     // 给meau添加滑动事件
     over() {
         let _this = this;
         for (let i = 0; i < this.meau.length; i++) {
-            // _this.index = i;
             this.meau[i].onmouseover = function () {
-                for (let j = 0; j < _this.meau.length; j++) {
-
-                    _this.meau[j].className = '';
-                    tool.bufferMove(_this.img[j], { opacity: 0 })
-                }
-                _this.meau[i].className = 'show';
-                tool.bufferMove(_this.img[i], { opacity: 100 })
+                _this.index = i;
+                _this.opacity();
             }
         }
     }
     // 给箭头添加点击事件
-    click() {
+    rightclick() {
         let _this = this;
-        this.btnright.onclick = function () {
-            _this.index++;
-            if (_this.index > _this.img.length-1) {
-                _this.index = 0;
-            }
-            tool.bufferMove(_this.img[_this.index], { opacity: 100 })
-            for (let j = 0; j < _this.meau.length; j++) {
-                _this.meau[j].className = '';
-                tool.bufferMove(_this.img[j], { opacity: 0 })
-            }
-            _this.meau[_this.index].className = 'show';
-            tool.bufferMove(_this.img[_this.index], { opacity: 100 })
-        }
-        this.btnleft.onclick = function () {
-            _this.index--;
-            if (_this.index < 0) {
-                _this.index = 5;
-            }
-            tool.bufferMove(_this.img[_this.index], { opacity: 100 })
-            for (let j = 0; j < _this.meau.length; j++) {
-                _this.meau[j].className = '';
-                tool.bufferMove(_this.img[j], { opacity: 0 })
-            }
-            _this.meau[_this.index].className = 'show';
-            tool.bufferMove(_this.img[_this.index], { opacity: 100 })
-        }
+        _this.index++;
+        if (_this.index > _this.img.length - 1) {
+            _this.index = 0;
+        };
+        tool.bufferMove(_this.img[_this.index], { opacity: 100 });
+        _this.opacity();
     }
+    leftclick() {
+        let _this = this;
+        _this.index--;
+        if (_this.index < 0) {
+            _this.index = 5;
+        }
+        tool.bufferMove(_this.img[_this.index], { opacity: 100 });
+        _this.opacity();
 
+    }
+    // 封装切换过程
+    opacity() {
+        for (let j = 0; j < this.meau.length; j++) {
+            this.meau[j].className = '';
+            tool.bufferMove(this.img[j], { opacity: 0 });
+        }
+        this.meau[this.index].className = 'show';
+        tool.bufferMove(this.img[this.index], { opacity: 100 });
+    }
+    // 自动轮播
+    automation() {
+        let _this = this;
+        setInterval(function () {
+            _this.rightclick()
+        }, 2500)
+    }
 };
 new Slide().init();
 
+// 热卖单品
+class Hot {
+    constructor() {
+        this.oLi = tool.$('.selling-item .inner .pic li', 'all');
+        this.oLine = tool.$('.selling-item .inner .line .line-con');
+        this.oDiv = tool.$('#box', 'all');
+        this.arr = ['10%', '35%', '60%', '85%']
+    }
+    init() {
+        this.over()
+    }
+    over() {
+        let _this = this;
+        for (let i = 0; i < this.oLi.length; i++) {
+            this.oLi[i].onmouseover = function () {
+                for (let j = 0; j < _this.oDiv.length; j++) {
+                    _this.oDiv[j].className = 'hid';
+                    _this.oDiv[j].style.display='none'
+                    tool.bufferMove(_this.oDiv[j], { opacity: 0 });
+                }
+                _this.oDiv[i].style.display = 'block';
+                tool.bufferMove(_this.oDiv[i], { opacity: 100 });
+                _this.oLine.style.left = _this.arr[i];
+                tool.bufferMove(_this.oLine,{left:_this.arr[i]})
+            }
+        }
+
+    }
+}
+new Hot().init();
 export { Slide, Navigation }
--- src/script/tool.js
+++ src/script/tool.js
@@ -88,7 +88,7 @@ function Tool() {
 
         // 添加类 addClass removeClass
 
-        addClass:function (ele, classname) { //ele:元素对象   
+        addClass: function (ele, classname) { //ele:元素对象   
             //判断ele是一个元素还是多个元素。
             //Node:一个元素
             //NodeList：多个元素
@@ -96,24 +96,117 @@ function Tool() {
                 for (let i = 0; i < ele.length; i++) {
                     ele[i].nodeType === 1 && (ele[i].className += (ele[i].className ? " " : "") + classname);
                 }
-            }else if(ele instanceof Node){
+            } else if (ele instanceof Node) {
                 ele.nodeType === 1 && (ele.className += (ele.className ? " " : "") + classname);
             }
         },
 
-        removeClass:function(elem, name) {
+        removeClass: function (elem, name) {
             var set = " " + elem.className + " ";
-  
+
             // Class name may appear multiple times
             while (set.indexOf(" " + name + " ") >= 0) {
                 set = set.replace(" " + name + " ", " ");
             }
-  
+
             // Trim for prettiness
             elem.className = typeof set.trim === "function" ? set.trim() : set.replace(/^\s+|\s+$/g, "");
+        },
+
+        // ajax
+        ajxa: function (obj) {
+
+            //将对象转换成数据拼接适合的字符串的格式?&
+            function objToString(obj) {
+                if (Object.prototype.toString.call(obj).slice(8, -1) === 'Object') {
+                    let objarr = [];
+                    for (let i in obj) {
+                        objarr.push(i + '=' + obj[i]);
+                    }
+                    return objarr.join('&')
+                }
+            }
+
+            //创建promise实例
+            let promise = new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest(); //获取ajax对象
+
+                //1.默认get,此参数可以省略。
+                obj.type = obj.type || 'get';
+
+                //2.接口地址不能为空
+                if (!obj.url) {
+                    throw new Error('接口地址不能为空');
+                }
+
+                //3.数据存在(对象转字符串，字符串直接使用)
+                if (obj.data) {
+                    if (Object.prototype.toString.call(obj.data).slice(8, -1) === 'Object') {
+                        obj.data = objToString(obj.data);
+                    } else {
+                        obj.data = obj.data;
+                    }
+                }
+
+                //4.数据存在，同时get请求，将数据拼接到地址栏的后面
+                if (obj.data && obj.type === 'get') {
+                    obj.url += '?' + obj.data;
+                }
+
+                //6.是否异步,如果同步无需onreadystatechange进行监听
+                if (obj.async === 'false' || obj.async === false) {
+                    obj.async = false;
+                } else {
+                    obj.async = true;
+                }
+
+                ajax.open(obj.type, obj.url, obj.async);
+
+                //5.数据存在和post,通过send和请求头传输数据
+                if (obj.data && obj.type === 'post') {
+                    ajax.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+                    ajax.send(obj.data);
+                } else {
+                    ajax.send();
+                }
+
+                //7.同步异步获取数据(利用回调函数将数据传输出来)
+                if (obj.async) { //异步
+                    ajax.onreadystatechange = function () {
+                        if (ajax.readyState === 4) { //服务器响应成功
+                            if (ajax.status === 200) { //请求接口成功
+                                let objdata = null;
+                                if (obj.dataType === 'json') {
+                                    objdata = JSON.parse(ajax.responseText);
+                                } else {
+                                    objdata = ajax.responseText;
+                                }
+                                resolve(objdata);
+
+
+                            } else { //接口有误
+                                reject('接口地址有误');
+                            }
+                        }
+                    }
+                } else { //同步
+                    if (ajax.status === 200) {
+                        let objdata = null;
+                        if (obj.dataType === 'json') {
+                            objdata = JSON.parse(ajax.responseText);
+                        } else {
+                            objdata = ajax.responseText;
+                        }
+                        resolve(objdata);
+                    } else {
+                        reject('接口地址有误');
+                    }
+                }
+
+            });
+
+            return promise;
         }
-
-
     }
 }
 
